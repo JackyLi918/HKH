a8cb1b5112920cde4b6a5c9415cf95294f6299ed
Jacky <jackyli.918@hotmail.com>
2018-09-21 13:13:42 +0800

    1.0.5.0

--- HKHProjects/DataProvider/DapperLinq/LinqAssemblyInfo.cs
+++ HKHProjects/DataProvider/DapperLinq/LinqAssemblyInfo.cs
@@ -12,6 +12,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
 //public token is c3a7a556298809fa if strong named
