49c132ff8c386086f1ad9f044d037cd0b2de1088
Jackyli <liwt@shinetechchina.com>
2020-04-13 17:04:43 +0800

    nuget push
    push common & csv to nugetserver

--- HKHProjects/HKH.Common/Properties/AssemblyInfo.cs
+++ HKHProjects/HKH.Common/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HKH.Common")]
-[assembly: AssemblyDescription("HKH Common classes")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jacky li")]
-[assembly: AssemblyProduct("HKH.Common")]
-[assembly: AssemblyCopyright("Copyright © BlackEyes 2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("HKH.Common")]
+//[assembly: AssemblyDescription("HKH Common classes")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Jacky li")]
+//[assembly: AssemblyProduct("HKH.Common")]
+//[assembly: AssemblyCopyright("Copyright © BlackEyes 2009")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.5.2.1")]
-[assembly: AssemblyFileVersion("4.5.2.1")]
+//[assembly: AssemblyVersion("4.5.2.1")]
+//[assembly: AssemblyFileVersion("4.5.2.1")]
 
 [assembly: InternalsVisibleTo("HKH.Common.Test")]
