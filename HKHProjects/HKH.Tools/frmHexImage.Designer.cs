d11e08dfda03fb22d599e82e4201d9a54dc4b828
Jacky Li <liwt@shinetechsoftware.com>
2022-07-21 21:31:30 +0800

    add Image/HexString convert form

---
+++ HKHProjects/HKH.Tools/frmHexImage.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace HKH.Tools
+{
+    partial class frmHexImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHexString = new System.Windows.Forms.TextBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnHexToImage = new System.Windows.Forms.Button();
+            this.btnImageToHex = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtHexString
+            // 
+            this.txtHexString.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtHexString.Location = new System.Drawing.Point(3, 3);
+            this.txtHexString.Multiline = true;
+            this.txtHexString.Name = "txtHexString";
+            this.tableLayoutPanel1.SetRowSpan(this.txtHexString, 4);
+            this.txtHexString.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtHexString.Size = new System.Drawing.Size(475, 619);
+            this.txtHexString.TabIndex = 0;
+            // 
+            // pbImage
+            // 
+            this.pbImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbImage.Location = new System.Drawing.Point(584, 3);
+            this.pbImage.Name = "pbImage";
+            this.tableLayoutPanel1.SetRowSpan(this.pbImage, 4);
+            this.pbImage.Size = new System.Drawing.Size(475, 619);
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // btnHexToImage
+            // 
+            this.btnHexToImage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnHexToImage.Location = new System.Drawing.Point(484, 238);
+            this.btnHexToImage.Name = "btnHexToImage";
+            this.btnHexToImage.Size = new System.Drawing.Size(94, 23);
+            this.btnHexToImage.TabIndex = 2;
+            this.btnHexToImage.Text = "Hex To Image >";
+            this.btnHexToImage.UseVisualStyleBackColor = true;
+            this.btnHexToImage.Click += new System.EventHandler(this.btnHexToImage_Click);
+            // 
+            // btnImageToHex
+            // 
+            this.btnImageToHex.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnImageToHex.Location = new System.Drawing.Point(484, 363);
+            this.btnImageToHex.Name = "btnImageToHex";
+            this.btnImageToHex.Size = new System.Drawing.Size(94, 23);
+            this.btnImageToHex.TabIndex = 3;
+            this.btnImageToHex.Text = "< Image To Hex";
+            this.btnImageToHex.UseVisualStyleBackColor = true;
+            this.btnImageToHex.Click += new System.EventHandler(this.btnImageToHex_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.txtHexString, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pbImage, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnImageToHex, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnHexToImage, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1062, 625);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // frmHexImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1086, 649);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmHexImage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片/字符串转换";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHexString;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnHexToImage;
+        private System.Windows.Forms.Button btnImageToHex;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
--- HKHProjects/HKH.Tools/frmMain.Designer.cs
+++ HKHProjects/HKH.Tools/frmMain.Designer.cs
@@ -28,67 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-			this.tsMain = new System.Windows.Forms.ToolStrip();
-			this.tsbAppSetting = new System.Windows.Forms.ToolStripButton();
-			this.tsbDbBackupAndRestore = new System.Windows.Forms.ToolStripButton();
-			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-			this.tsMain.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// tsMain
-			// 
-			this.tsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tsMain = new System.Windows.Forms.ToolStrip();
+            this.tsbAppSetting = new System.Windows.Forms.ToolStripButton();
+            this.tsbDbBackupAndRestore = new System.Windows.Forms.ToolStripButton();
+            this.tsbImageConvert = new System.Windows.Forms.ToolStripButton();
+            this.tsMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tsMain
+            // 
+            this.tsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsbAppSetting,
             this.tsbDbBackupAndRestore,
-            this.toolStripButton1});
-			this.tsMain.Location = new System.Drawing.Point(0, 0);
-			this.tsMain.Name = "tsMain";
-			this.tsMain.Size = new System.Drawing.Size(511, 25);
-			this.tsMain.TabIndex = 0;
-			this.tsMain.Text = "toolStrip1";
-			// 
-			// tsbAppSetting
-			// 
-			this.tsbAppSetting.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.tsbAppSetting.Image = ((System.Drawing.Image)(resources.GetObject("tsbAppSetting.Image")));
-			this.tsbAppSetting.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.tsbAppSetting.Name = "tsbAppSetting";
-			this.tsbAppSetting.Size = new System.Drawing.Size(23, 22);
-			this.tsbAppSetting.Text = "应用程序配置";
-			this.tsbAppSetting.Click += new System.EventHandler(this.tsbAppSetting_Click);
-			// 
-			// tsbDbBackupAndRestore
-			// 
-			this.tsbDbBackupAndRestore.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.tsbDbBackupAndRestore.Image = ((System.Drawing.Image)(resources.GetObject("tsbDbBackupAndRestore.Image")));
-			this.tsbDbBackupAndRestore.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.tsbDbBackupAndRestore.Name = "tsbDbBackupAndRestore";
-			this.tsbDbBackupAndRestore.Size = new System.Drawing.Size(23, 22);
-			this.tsbDbBackupAndRestore.Text = "数据库备份还原";
-			// 
-			// toolStripButton1
-			// 
-			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButton1.Name = "toolStripButton1";
-			this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton1.Text = "toolStripButton1";
-			this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
-			// 
-			// frmMain
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(511, 425);
-			this.Controls.Add(this.tsMain);
-			this.Name = "frmMain";
-			this.Text = "黑瞳工具箱";
-			this.tsMain.ResumeLayout(false);
-			this.tsMain.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.tsbImageConvert});
+            this.tsMain.Location = new System.Drawing.Point(0, 0);
+            this.tsMain.Name = "tsMain";
+            this.tsMain.Size = new System.Drawing.Size(511, 25);
+            this.tsMain.TabIndex = 0;
+            this.tsMain.Text = "toolStrip1";
+            // 
+            // tsbAppSetting
+            // 
+            this.tsbAppSetting.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbAppSetting.Image = ((System.Drawing.Image)(resources.GetObject("tsbAppSetting.Image")));
+            this.tsbAppSetting.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbAppSetting.Name = "tsbAppSetting";
+            this.tsbAppSetting.Size = new System.Drawing.Size(23, 22);
+            this.tsbAppSetting.Text = "应用程序配置";
+            this.tsbAppSetting.Click += new System.EventHandler(this.tsbAppSetting_Click);
+            // 
+            // tsbDbBackupAndRestore
+            // 
+            this.tsbDbBackupAndRestore.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbDbBackupAndRestore.Image = ((System.Drawing.Image)(resources.GetObject("tsbDbBackupAndRestore.Image")));
+            this.tsbDbBackupAndRestore.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDbBackupAndRestore.Name = "tsbDbBackupAndRestore";
+            this.tsbDbBackupAndRestore.Size = new System.Drawing.Size(23, 22);
+            this.tsbDbBackupAndRestore.Text = "数据库备份还原";
+            // 
+            // tsbImageConvert
+            // 
+            this.tsbImageConvert.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbImageConvert.Image = ((System.Drawing.Image)(resources.GetObject("tsbImageConvert.Image")));
+            this.tsbImageConvert.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbImageConvert.Name = "tsbImageConvert";
+            this.tsbImageConvert.Size = new System.Drawing.Size(23, 22);
+            this.tsbImageConvert.Text = "图片字符串转换";
+            this.tsbImageConvert.Click += new System.EventHandler(this.tsbImageConvert_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 425);
+            this.Controls.Add(this.tsMain);
+            this.Name = "frmMain";
+            this.Text = "黑瞳工具箱";
+            this.tsMain.ResumeLayout(false);
+            this.tsMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,7 +97,7 @@
         private System.Windows.Forms.ToolStrip tsMain;
         private System.Windows.Forms.ToolStripButton tsbAppSetting;
         private System.Windows.Forms.ToolStripButton tsbDbBackupAndRestore;
-		private System.Windows.Forms.ToolStripButton toolStripButton1;
+		private System.Windows.Forms.ToolStripButton tsbImageConvert;
     }
 }
 
