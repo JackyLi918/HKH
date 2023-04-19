1c5edfd1ed49ea61f31bddf532427fd191f5bca4
Jacky Li <jackyli.918@hotmail.com>
2023-04-19 17:48:30 +0800

    add bcrypt

---
+++ HKHProjects/UnitTest/HKH.Common.Test/Security/BCryptTest.cs
@@ -0,0 +1,21 @@
+﻿using HKH.Common.Security;
+
+namespace HKH.Common.Security.Test
+{
+    [TestClass]
+    public class BCryptTest
+    {
+         [TestMethod]
+        public void TestHashAnd()
+        {
+            string test = "I'm testing BCrypt encryption.";
+
+            string encrypt1=BCrypt.HashPassword(test);
+            string encrypt2 = BCrypt.HashPassword(test);
+
+            Assert.AreNotEqual(encrypt1, encrypt2);
+            Assert.IsTrue(BCrypt.Verify(test,encrypt1));
+            Assert.IsTrue(BCrypt.Verify(test, encrypt2));
+        }
+    }
+}
