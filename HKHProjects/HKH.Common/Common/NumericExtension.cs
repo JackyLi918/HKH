a6d77cd3ade601ea227068fab2b788111b67db46
Jacky Li <jackyli.918@hotmail.com>
2023-08-08 11:07:15 +0800

    add extension

---
+++ HKHProjects/HKH.Common/Common/NumericExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace System//HKH.Common
+{
+    public static class NumericExtension
+    {
+        /// <summary>
+        /// Parse json ticks to local datetime
+        /// </summary>
+        /// <param name="ticks"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this long ticks)
+        {
+            var dtStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+            return dtStart.AddTicks(ticks * 10000000);
+        }
+    }
+}
